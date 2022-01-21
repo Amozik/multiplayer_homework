7de3460edcf3cd99762e8d0f3d442430929ec56c
Amozik <Amozik@rambler.ru>
2022-01-21 22:18:48 +0300

    hlapi example

---
+++ Assets/Scripts/FireAction.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class FireAction : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    [SerializeField]
+    private int _startAmmunition = 20;
+
+    public string BulletCount { get; protected set; } = string.Empty;
+    protected Queue<GameObject> _bullets = new Queue<GameObject>();
+    protected Queue<GameObject> _ammunition = new Queue<GameObject>();
+    protected bool _reloading = false;
+
+    protected virtual void Start()
+    {
+        for (var i = 0; i < _startAmmunition; i++)
+        {
+            GameObject bullet;
+            if (_bulletPrefab == null)
+            {
+                bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                bullet.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+            else
+            {
+                bullet = Instantiate(_bulletPrefab);
+            }
+            bullet.SetActive(false);
+            _ammunition.Enqueue(bullet);
+        }
+    }
+
+    public virtual async void Reloading()
+    {
+        _bullets = await Reload();
+    }
+
+    protected virtual void Shooting()
+    {
+        if (_bullets.Count == 0)
+        {
+            Reloading();
+        }
+    }
+
+    private async Task<Queue<GameObject>> Reload()
+    {
+        if (!_reloading)
+        {
+            _reloading = true;
+            StartCoroutine(ReloadingAnim());
+            return await Task.Run(delegate
+            {
+                var cage = 10;
+                if (_bullets.Count < cage)
+                {
+                    Thread.Sleep(3000);
+                    var bullets = this._bullets;
+                    while (bullets.Count > 0)
+                    {
+                        _ammunition.Enqueue(bullets.Dequeue());
+                    }
+                    cage = Mathf.Min(cage, _ammunition.Count);
+                    if (cage > 0)
+                    {
+                        for (var i = 0; i < cage; i++)
+                        {
+                            var sphere = _ammunition.Dequeue();
+                            bullets.Enqueue(sphere);
+                        }
+                    }
+                }
+                _reloading = false;
+                return _bullets;
+            });
+        }
+        else
+        {
+            return _bullets;
+        }
+    }
+
+    private IEnumerator ReloadingAnim()
+    {
+        while (_reloading)
+        {
+            BulletCount = " | ";
+            yield return new WaitForSeconds(0.01f);
+            BulletCount = @" \ ";
+            yield return new WaitForSeconds(0.01f);
+            BulletCount = "---";
+            yield return new WaitForSeconds(0.01f);
+            BulletCount = " / ";
+            yield return new WaitForSeconds(0.01f);
+        }
+        BulletCount = _bullets.Count.ToString();
+        yield return null;
+    }
+}
+
